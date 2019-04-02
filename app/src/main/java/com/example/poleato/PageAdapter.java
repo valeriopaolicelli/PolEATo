a4d60794718a707da4fec78be6c5f84ce80b7fdb
Valerio Paolicelli <valerio.paolicelli.vp@gmail.com>
2019-04-02 18:25:27 +0200

    Page adapter

---
+++ app/src/main/java/com/example/poleato/PageAdapter.java
@@ -0,0 +1,36 @@
+package com.example.poleato;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PageAdapter extends FragmentPagerAdapter {
+    private List<Fragment> fragmentList= new ArrayList<>();
+    private List<String> fragmentNameList= new ArrayList<>();
+
+    public PageAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    public void addFragment(Fragment fragment, String nameFragment){
+        fragmentList.add(fragment);
+        fragmentNameList.add(nameFragment);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return fragmentList.get(i);
+    }
+
+    public String getFragmentName(int i){
+        return fragmentNameList.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return fragmentList.size();
+    }
+}
