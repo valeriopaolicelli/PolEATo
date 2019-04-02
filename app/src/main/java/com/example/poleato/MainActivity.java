e2617115fe4cd8d62a3afe4a72c51f824da675d1
FabioRiccio <fabio.ricciardi@studenti.polito.it>
2019-04-02 17:12:10 +0200

    xml file

--- app/src/main/java/com/example/poleato/MainActivity.java
+++ app/src/main/java/com/example/poleato/MainActivity.java
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(getSupportActionBar() != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setTitle("Your Restaurant");
+        }
     }
 }
