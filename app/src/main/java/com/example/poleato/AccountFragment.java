4b6f07cbcf9ab88296c1b12badc3171a467460fc
Valerio Paolicelli <valerio.paolicelli.vp@gmail.com>
2019-04-03 11:17:09 +0200

    minor

--- app/src/main/java/com/example/poleato/AccountFragment.java
+++ app/src/main/java/com/example/poleato/AccountFragment.java
@@ -5,15 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
-
-/**
- * Created by User on 2/28/2017.
- */
 
 public class AccountFragment extends Fragment {
-    private static final String TAG = "Tab1Fragment";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
--- app/src/main/java/com/example/poleato/DailyOfferFragment.java
+++ app/src/main/java/com/example/poleato/DailyOfferFragment.java
@@ -1,22 +1,14 @@
 package com.example.poleato;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.Toast;
 
-/**
- * Created by User on 2/28/2017.
- */
 
 public class DailyOfferFragment extends Fragment {
-    private static final String TAG = "Tab1Fragment";
 
-    @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.offer_frag_layout,container,false);
