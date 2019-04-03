a620406dff5679d0d69473dc2f3e992f39910efb
Michelangelo Moncada <s255544@studenti.polito.it>
2019-04-03 18:36:41 +0200

    Added list manager

--- app/src/main/java/com/example/poleato/DailyOfferFragment.java
+++ app/src/main/java/com/example/poleato/DailyOfferFragment.java
@@ -5,13 +5,45 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 
 public class DailyOfferFragment extends Fragment {
 
+
+    private ListView itemList;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.offer_frag_layout,container,false);
+        itemList = view.findViewById(R.id.listItem);
+
+        itemList.setAdapter(new BaseAdapter() {
+            @Override
+            public int getCount() {
+                return 0;
+            }
+
+            @Override
+            public Object getItem(int position) {
+                return null;
+            }
+
+            @Override
+            public long getItemId(int position) {
+                return 0;
+            }
+
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+
+
+                return null;
+            }
+        });
 
         return view;
     }
