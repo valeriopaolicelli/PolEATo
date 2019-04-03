f2a10e793404b366239815e034ce2a17f3ae2d16
Matteo Senese <matteo.senese@gmail.com>
2019-04-03 19:03:10 +0200

    java code for vector filling added

--- app/src/main/java/com/example/poleato/DailyOfferFragment.java
+++ app/src/main/java/com/example/poleato/DailyOfferFragment.java
@@ -5,46 +5,81 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
-import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.widget.ExpandableListView;
+import com.example.poleato.ExpandableListManagement.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class DailyOfferFragment extends Fragment {
 
+    ExpandableListAdapter listAdapter;
+    ExpandableListView expListView;
+    List<String> listDataHeader;
+    HashMap<String, List<String>> listDataChild;
 
-    private ListView itemList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.offer_frag_layout,container,false);
-        itemList = view.findViewById(R.id.listItem);
+        View view = inflater.inflate(R.layout.offer_frag_layout, container, false);
 
-        itemList.setAdapter(new BaseAdapter() {
-            @Override
-            public int getCount() {
-                return 0;
-            }
+        // get the listview
+        expListView = (ExpandableListView) view.findViewById(R.id.expandableList);
 
-            @Override
-            public Object getItem(int position) {
-                return null;
-            }
+        // preparing list data
+        prepareListData();
 
-            @Override
-            public long getItemId(int position) {
-                return 0;
-            }
+        listAdapter = new ExpandableListAdapter(this.getContext(), listDataHeader, listDataChild);
 
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-
-
-                return null;
-            }
-        });
+        // setting list adapter
+        expListView.setAdapter(listAdapter);
 
         return view;
     }
+
+    /*
+     * Preparing the list data
+     */
+    private void prepareListData() {
+        listDataHeader = new ArrayList<String>();
+        listDataChild = new HashMap<String, List<String>>();
+
+        // Adding child data
+        listDataHeader.add("Top 250");
+        listDataHeader.add("Now Showing");
+        listDataHeader.add("Coming Soon..");
+
+        // Adding child data
+        List<String> top250 = new ArrayList<String>();
+        top250.add("The Shawshank Redemption");
+        top250.add("The Godfather");
+        top250.add("The Godfather: Part II");
+        top250.add("Pulp Fiction");
+        top250.add("The Good, the Bad and the Ugly");
+        top250.add("The Dark Knight");
+        top250.add("12 Angry Men");
+
+        List<String> nowShowing = new ArrayList<String>();
+        nowShowing.add("The Conjuring");
+        nowShowing.add("Despicable Me 2");
+        nowShowing.add("Turbo");
+        nowShowing.add("Grown Ups 2");
+        nowShowing.add("Red 2");
+        nowShowing.add("The Wolverine");
+
+        List<String> comingSoon = new ArrayList<String>();
+        comingSoon.add("2 Guns");
+        comingSoon.add("The Smurfs 2");
+        comingSoon.add("The Spectacular Now");
+        comingSoon.add("The Canyons");
+        comingSoon.add("Europa Report");
+
+        listDataChild.put(listDataHeader.get(0), top250); // Header, Child data
+        listDataChild.put(listDataHeader.get(1), nowShowing);
+        listDataChild.put(listDataHeader.get(2), comingSoon);
+    }
+
 }
+
