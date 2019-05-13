3b404bfc9aeaec03ebd873862a0fa3ab3011ec8f
Michelangelo Moncada <s255544@studenti.polito.it>
2019-05-13 16:45:01 +0200

    logIn bug fix

--- customer/src/main/java/com/mad/poleato/SignUpActivity.java
+++ customer/src/main/java/com/mad/poleato/SignUpActivity.java
@@ -198,6 +198,7 @@ public class SignUpActivity extends AppCompatActivity {
     public void access(){
         Intent myIntent = new Intent(SignUpActivity.this, NavigatorActivity.class);
         SignUpActivity.this.startActivity(myIntent);
+        finish();
     }
 
 
@@ -321,8 +322,6 @@ public class SignUpActivity extends AppCompatActivity {
                     break;
 
                 case R.id.ButtonSignIn:
-                    Intent myIntent = new Intent(SignUpActivity.this, SignInActivity.class);
-                    SignUpActivity.this.startActivity(myIntent);
                     finish();
                     break;
 
--- restaurant/src/main/java/com/mad/poleato/SignUpActivity.java
+++ restaurant/src/main/java/com/mad/poleato/SignUpActivity.java
@@ -196,6 +196,7 @@ public class SignUpActivity extends AppCompatActivity {
     public void access(){
         Intent myIntent = new Intent(SignUpActivity.this, NavigatorActivity.class);
         SignUpActivity.this.startActivity(myIntent);
+        finish();
     }
 
 
@@ -320,8 +321,6 @@ public class SignUpActivity extends AppCompatActivity {
                     break;
 
                 case R.id.ButtonSignIn:
-                    Intent myIntent = new Intent(SignUpActivity.this, SignInActivity.class);
-                    SignUpActivity.this.startActivity(myIntent);
                     finish();
                     break;
 
